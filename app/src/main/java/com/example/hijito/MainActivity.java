4da3e8dd1a0fafcc20478b8151d4aad23f6c377a
yelsin fabian lopez <yelsincito@gmail.com>
2019-11-12 22:28:41 -0500

    segundo commit

--- app/src/main/java/com/example/hijito/MainActivity.java
+++ app/src/main/java/com/example/hijito/MainActivity.java
@@ -2,15 +2,42 @@ package com.example.hijito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
+    private Button btnProducts;
+    private  Button btnAboutUs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // se enlasa el activity + layout
+        btnProducts = (Button) findViewById(R.id.btnEnter);
+        btnAboutUs = (Button) findViewById(R.id.btnAboutUs);
+        btnProducts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Log.i(Settings.INFO, "Products");
+                // intent sirve para cambear de pantalla
+                Intent i = new Intent(MainActivity.this, ProducListActivity.class); //para saltar ala siguiente pantalla
+                startActivity(i);
+            }
+        });
+        btnAboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.i(Settings.INFO,"About us");
+                Intent i = new Intent(MainActivity.this, AboutUsActivity.class);
+                startActivity(i);
+            }
+        });
     }
     @Override
     protected void onResume(){
